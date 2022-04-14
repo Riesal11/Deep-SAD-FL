db9d2877fd7df3fdf8ba60ebf3dfd2281587dc5c
veronikaBek <veronika.bekbulatova@egmail.com>
2022-04-14 15:47:16 +0200

    too many config variables for the tuning

--- src/tune.py
+++ src/tune.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ray
 from ray import tune
+import numpy as np
 
 
 from datasets.main import load_dataset
@@ -8,17 +9,20 @@ from DeepSAD import DeepSAD
 
 def train_tune(config,checkpoint_dir=None):
     
-    dataset = load_dataset('iiot', config['data_path'],config['fl_dataset_index'], config['dataset_size'],config['net_name'],config['normal_class'], config['known_outlier_class'], config['n_known_outlier_classes'],
-    config['ratio_known_outlier'], config['ratio_pollution'], random_state=config['random_state'])
-    deepSAD = DeepSAD(config['eta'])
-    deepSAD.set_network(config['net_name'],h1=config['h1']) #first hidden layer of the neuronal network
+    #dataset = load_dataset('iiot', config['data_path'],config['fl_dataset_index'], config['dataset_size'],config['net_name'],config['normal_class'], config['known_outlier_class'], config['n_known_outlier_classes'],
+    #config['ratio_known_outlier'], config['ratio_pollution'], random_state=config['random_state'])
+    dataset = load_dataset('iiot', data_path='/content/drive/Othercomputers/My laptop/Deep-SAD-FL/data',
+              net_name='iiot_no_cat',normal_class=0, known_outlier_class=1,n_known_outlier_classes=1, 
+              ratio_known_outlier=0.01,ratio_pollution=0.05,random_state=np.random.RandomState(1))
+    deepSAD = DeepSAD(eta=1)
+    deepSAD.set_network('iiot_no_cat',h1=config['h1']) #first hidden layer of the neuronal network
 
-    deepSAD.train(dataset,device=config['device'], lr=config['lr'], 
-        optimizer_name=config['optimizer_name'], n_epochs=config['n_epochs'], 
-        lr_milestones=config['lr_milestone'], batch_size=config['bs'], 
-        weight_decay=config['weight_decay'],n_jobs_dataloader=config['n_jobs_dataloader']) 
+    deepSAD.train(dataset,device='cuda', lr=config['lr'], 
+        optimizer_name='adam', n_epochs=50, 
+        lr_milestones=(25,), batch_size=config['bs'], 
+        weight_decay=5e-07) 
         #optimize for the learning rate and batch size
     
-    deepSAD.test(dataset, device=config['device'], n_jobs_dataloader=config['n_jobs_dataloader'])
+    deepSAD.test(dataset, device='cuda')
 
     tune.report(f_score=deepSAD.results['test_f1'])
